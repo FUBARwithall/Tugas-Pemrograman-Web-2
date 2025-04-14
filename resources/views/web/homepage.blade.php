2b9e2a34608f6a4dd4f91f91e8924bf2e4a18874
JustParadis <panjirafi96@gmail.com>
2025-04-14 08:39:35 +0700

    [Pertemuan 4]

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -1,35 +1,20 @@
 <x-layout>
-    <x-slot:title>{{ $title }}</x-slot>
-
-    <h3>Ini adalah halaman Homepage</h3>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
-    <div class="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
-    </div>
-    <div class="alert alert-secondary" role="alert">
-        A simple secondary alert—check it out!
-    </div>
-    <div class="alert alert-success" role="alert">
-        A simple success alert—check it out!
-    </div>
-    <div class="alert alert-danger" role="alert">
-        A simple danger alert—check it out!
-    </div>
-    <div class="alert alert-warning" role="alert">
-        A simple warning alert—check it out!
-    </div>
-    <div class="alert alert-info" role="alert">
-        A simple info alert—check it out!
-    </div>
-    <div class="alert alert-light" role="alert">
-        A simple light alert—check it out!
-    </div>
-    <div class="alert alert-dark" role="alert">
-        A simple dark alert—check it out!
+    <div class="row">
+        <h3>Categories</h3>
+        @foreach($categories as $category)
+            <div class="col-2">
+                <div class="card">
+                    <img src="{{ $category['image'] }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $category['name'] }}</h5>
+                        <p class="card-text">
+                            {{ $category['description'] }}
+                        </p>
+                        <a href="/category/{{ $category['slug'] }}" class="btn
+                        btn-primary">Detail</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </x-layout>
